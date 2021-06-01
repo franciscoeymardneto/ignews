368bfaf7c676ec8da7da27856f02f1efd6ed3380
netoeymard <eymard@sysautomacao.com.br>
2021-06-01 20:26:08 -0300

    feat: read request buffer

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from 'stream'
 
+async function buffer(readble: Readable) {
+    const chunks = []
+
+    for await (const chunck of readble) {
+        chunks.push(
+            typeof chunck === 'string' ? Buffer.from(chunck) : chunck
+        )
+    }
+
+    return Buffer.concat(chunks)
+}
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log('evento recebido');
+    const buf = await buffer(req)
 
     res.status(200).json({ ok: true})
     
