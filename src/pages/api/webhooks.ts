d4015cb7e88dc54bdeadcfcebdda5e79adf05a12
netoeymard <eymard@sysautomacao.com.br>
2021-06-01 20:00:54 -0300

    feat: listen webhooks stripe

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    console.log('evento recebido');
+
+    res.status(200).json({ ok: true})
+    
+}
